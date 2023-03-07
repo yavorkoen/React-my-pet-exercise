a7c9f3cc81455ab129e8c68be501608d605e91dc
Yavor Koen <yavorkoen@gmail.com>
2023-03-07 11:27:11 +0200

    add Header component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div classNameName="App">
       <div id="container">
         
-        <header id="site-header">
-            <nav className="navbar">
-                <section className="navbar-dashboard">
-                    <a href="#">Dashboard</a>
-                    <div id="guest">
-                        <a className="button" href="#">Login</a>
-                        <a className="button" href="#">Register</a>
-                    </div>
-                    <div id="user">
-                        <span>Welcome, email</span>
-                        <a className="button" href="#">My Pets</a>
-                        <a className="button" href="#">Add Pet</a>
-                        <a className="button" href="#">Logout</a>
-                    </div>
-                </section>
-            </nav>
-        </header>
-
+        
+        < Header />
         <main id="site-content"></main>
 
         <section id="login-page" className="login">
